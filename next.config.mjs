1286d709184b7b31b0f40991fadea926d07f1d81
Mac Studio Hub <macstudiohub@Macs-MacBook-Air.local>
2026-04-01 17:30:02 +0530

    Optimize: Further reduce memory and speed up production build

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
+  productionBrowserSourceMaps: false,
+  swcMinify: true,
   experimental: {
     webpackBuildWorker: true,
     parallelServerCompiles: true,
